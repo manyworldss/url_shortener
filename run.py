94f2c5a441ed10f96944f55e480a03951c2dba8c
manyworldss <Ralphsuber@gmail.com>
2024-12-26 23:28:32 -0500

    first commit

---
+++ run.py
@@ -0,0 +1,7 @@
+from app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)  # Auto-reload when you make changes
+                        # Show detailed error messages
+                        # Help during development 
+                        # Make sure to turn off before production debug=False
